f2ea879c06da1150a94af73fc94f29349f9a55a8
theasadse <theasadse@gmail.com>
2025-12-30 10:08:27 +0500

    fixed issues

--- src/components/BusinessDealComponents/structure/BankAccountDetails.jsx
+++ src/components/BusinessDealComponents/structure/BankAccountDetails.jsx
@@ -1,54 +1,50 @@
-import { Flex, Typography } from 'antd'
-import { MaskedAccount } from '../../Ui/MaskedAccount'
-import { useQuery } from '@apollo/client';
-import { message,Spin } from "antd";
-import { GETUSERACTIVEBANK } from '../../../graphql/query';
+import { Flex, Typography } from "antd";
+import { MaskedAccount } from "../../Ui/MaskedAccount";
+import { useQuery } from "@apollo/client";
+import { Spin } from "antd";
+import { GETUSERACTIVEBANK } from "../../../graphql/query";
 
-
-const { Text } = Typography
-const BankAccountDetails = ({details}) => {
-    const [messageApi, contextHolder] = message.useMessage();
-    const { data:bankData, loading } = useQuery(GETUSERACTIVEBANK, {
-        variables: { 
-            getUserActiveBanksId:details?.busines?.seller?.id
-        },
-    });
-    const buyerBank = bankData?.getUserActiveBanks
-    if (loading) {
-        return (
-            <Flex justify="center" align="center" className='h-200'>
-                <Spin size="large" />
-            </Flex>
-        );
-    }
-
+const { Text } = Typography;
+const BankAccountDetails = ({ details }) => {
+  const { data: bankData, loading } = useQuery(GETUSERACTIVEBANK, {
+    variables: {
+      getUserActiveBanksId: details?.busines?.seller?.id,
+    },
+  });
+  const buyerBank = bankData?.getUserActiveBanks;
+  if (loading) {
     return (
-        <>
-        {contextHolder}
-        <Flex vertical gap={10}>
-            <Text className="fw-600 text-medium-gray fs-13">Bank Account</Text>
-            {buyerBank ? (
-                <div className="deals-status w-100 sky-lightest rounded-12">
-                    <Flex vertical gap={6}>
-                        <Text className="fs-15 fw-500 text-gray">
-                            {buyerBank.bankName}
-                        </Text>
-                        <Text className="fs-13 text-gray">
-                            Account Title: {buyerBank.accountTitle}
-                        </Text>
-                        <MaskedAccount
-                            iban={buyerBank.iban}
-                            className="fs-13 text-gray"
-                        />
-                    </Flex>
-                </div>
-            
-        ) : (
-            <Text className="fs-13 text-gray">No bank accounts available</Text>
-        )}
-        </Flex>
-        </>
-    )
-}
+      <Flex justify="center" align="center" className="h-200">
+        <Spin size="large" />
+      </Flex>
+    );
+  }
 
-export {BankAccountDetails}
+  return (
+    <>
+      <Flex vertical gap={10}>
+        <Text className="fw-600 text-medium-gray fs-13">Bank Account</Text>
+        {buyerBank ? (
+          <div className="deals-status w-100 sky-lightest rounded-12">
+            <Flex vertical gap={6}>
+              <Text className="fs-15 fw-500 text-gray">
+                {buyerBank.bankName}
+              </Text>
+              <Text className="fs-13 text-gray">
+                Account Title: {buyerBank.accountTitle}
+              </Text>
+              <MaskedAccount
+                iban={buyerBank.iban}
+                className="fs-13 text-gray"
+              />
+            </Flex>
+          </div>
+        ) : (
+          <Text className="fs-13 text-gray">No bank accounts available</Text>
+        )}
+      </Flex>
+    </>
+  );
+};
+
+export { BankAccountDetails };
